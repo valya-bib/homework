b0830f394d47623a9f1f4967053b153ca7cbee40
valya-bib <vzykova23@gmail.com>
2024-12-11 04:04:55 +0200

    Add files via upload

---
+++ 11.1.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexContainsOne = @"1";
+        string regexStartsWithOne = @"^1.*$";
+
+        bool containsOneMatch = Regex.IsMatch(input, regexContainsOne);
+        bool startsWithOneMatch = Regex.IsMatch(input, regexStartsWithOne);
+
+        Console.WriteLine($"строка содержит 1: {containsOneMatch}");
+        Console.WriteLine($"строка начинается с 1: {startsWithOneMatch}");
+    }
+}
