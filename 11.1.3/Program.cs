15eb57cad6b665e161568918049c0dcfcc91e231
valya-bib <vzykova23@gmail.com>
2024-12-11 04:04:24 +0200

    Add files via upload

---
+++ 11.1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexStartsWith = @"^101.*$";
+        string regexEndsWith = @".*101$";
+
+        bool isStartsWithMatch = Regex.IsMatch(input, regexStartsWith);
+        bool isEndsWithMatch = Regex.IsMatch(input, regexEndsWith);
+
+        Console.WriteLine($"строка начинается с 101': {isStartsWithMatch}");
+        Console.WriteLine($"строка заканчивается на 101': {isEndsWithMatch}");
+    }
+}
