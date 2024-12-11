7ec9e59737c07c409e28732cd86bb8d2b4ae4582
valya-bib <vzykova23@gmail.com>
2024-12-11 04:03:51 +0200

    Add files via upload

---
+++ 11.1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexWholeString = @"^.*101.*$";
+        string regexPartString = @"101";
+
+        bool isWholeStringMatch = Regex.IsMatch(input, regexWholeString);
+        bool isPartStringMatch = Regex.IsMatch(input, regexPartString);
+
+        Console.WriteLine($"строка соответствует целиком: {isWholeStringMatch}");
+        Console.WriteLine($"строка содержит 101 подряд: {isPartStringMatch}");
+    }
+}
