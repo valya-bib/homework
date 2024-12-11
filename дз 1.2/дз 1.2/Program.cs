23adf63089b899fcc7ec28c08c258cfa2902d648
valya-bib <vzykova23@gmail.com>
2024-12-11 03:53:43 +0200

    Add files via upload

---
+++ дз 1.2/дз 1.2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int chast = 0;
+while (a > b && b != 0)
+{
+    a = a - b;
+    chast += 1;
+}
+Console.WriteLine(chast);
