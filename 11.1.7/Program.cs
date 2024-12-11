6eac140f01736d50e48fb685dd7f85a1403179cd
valya-bib <vzykova23@gmail.com>
2024-12-11 04:06:34 +0200

    Add files via upload

---
+++ 11.1.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexFirstDigitUnique = @"^(\d)(?!.*\1)\d*$";
+        string regexOnlyDigits = @"^\d+$";
+
+        bool isOnlyDigits = Regex.IsMatch(input, regexOnlyDigits);
+        bool firstDigitUnique = isOnlyDigits && Regex.IsMatch(input, regexFirstDigitUnique);
+
+        Console.WriteLine(firstDigitUnique);
+    }
+}
