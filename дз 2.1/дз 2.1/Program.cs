5aacca3478cc0496ce39adaca6c198b4abcb0d98
valya-bib <vzykova23@gmail.com>
2024-12-11 03:50:02 +0200

    Add files via upload

---
+++ дз 2.1/дз 2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string S = "ababcabcab";
+        string S1 = "abc";
+
+        int count = CountOccurrences(S, S1);
+
+        Console.WriteLine($"количество вхождений '{S1}' в '{S}': {count}");
+    }
+
+    static int CountOccurrences(string S, string S1)
+    {
+        int count = 0;
+        int index = 0;
+
+        while ((index = S.IndexOf(S1, index)) != -1)
+        {
+            count++;
+            index += S1.Length;
+        }
+
+        return count;
+    }
+}
