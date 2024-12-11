f0b7c562da53c30b1bf7df23bf5855bfd78d4dbd
valya-bib <vzykova23@gmail.com>
2024-12-11 04:02:42 +0200

    Add files via upload

---
+++ дз 11.1.1/дз 11.1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexWholeString = @"^.*000.*$";
+        string regexPartString = @"000";
+
+        bool isWholeStringMatch = Regex.IsMatch(input, regexWholeString);
+        bool isPartStringMatch = Regex.IsMatch(input, regexPartString);
+
+        Console.WriteLine($"строка соответствует целиком: {isWholeStringMatch}");
+        Console.WriteLine($"строка содержит три нуля подряд: {isPartStringMatch}");
+    }
+}
