e19a801ce7fbdf41ef8de67ab387d020a2db7cbf
valya-bib <vzykova23@gmail.com>
2024-12-11 04:06:05 +0200

    Add files via upload

---
+++ 11.1.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexFirstDigitRepeated = @"^(\d)(?=.*\1)\d*$";
+        string regexOnlyDigits = @"^\d+$";
+
+        bool isOnlyDigits = Regex.IsMatch(input, regexOnlyDigits);
+        bool firstDigitRepeated = isOnlyDigits && Regex.IsMatch(input, regexFirstDigitRepeated);
+
+        Console.WriteLine(firstDigitRepeated);
+    }
+}
