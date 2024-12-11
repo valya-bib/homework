2a2d9f2116167bf787d4430a51f9b8eaea1de108
valya-bib <vzykova23@gmail.com>
2024-12-11 03:58:06 +0200

    Add files via upload

---
+++ дз 3.2/дз 3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int radius = 10; 
+        double s = 2.0; // cоотношение сторон
+
+        for (double y = -radius; y <= radius; y++)
+        {
+            for (double x = -radius * s; x <= radius * s; x++)
+            {
+                if (Math.Pow(x / s, 2) + Math.Pow(y, 2) <= Math.Pow(radius, 2))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
