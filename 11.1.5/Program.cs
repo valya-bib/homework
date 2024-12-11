6d902a214329896cd8c372fe6e6ed29940e4ca14
valya-bib <vzykova23@gmail.com>
2024-12-11 04:05:27 +0200

    Add files via upload

---
+++ 11.1.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("строка для проверки:");
+        string input = Console.ReadLine();
+
+        string regexNoOne = @"^[^1]*$";
+        string regexNoOnePart = @"[^1]";
+
+        bool noOneMatch = Regex.IsMatch(input, regexNoOne);
+        bool noOnePartMatch = Regex.IsMatch(input, regexNoOnePart);
+
+        Console.WriteLine($"строка не содержит 1: {noOneMatch}");
+        Console.WriteLine($"строка содержит хотя бы один символ, не равный 1: {noOnePartMatch}");
+    }
+}
