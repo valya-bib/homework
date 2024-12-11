4dc627918c9f3fcc8597c4e71dc83f80062c86c2
valya-bib <vzykova23@gmail.com>
2024-12-11 03:54:50 +0200

    Add files via upload

---
+++ дз 1.5/дз 1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿int a1 = Convert.ToInt32(Console.ReadLine());
+int a2 = Convert.ToInt32(Console.ReadLine());
+int a3 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int b3 = Convert.ToInt32(Console.ReadLine());
+if ( a1 / b1 == a2 / b2 && a2 / b2 == a3 / b3)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
