ada79a50cefc2aefd95ddc7aff2af5d4df3641b8
valya-bib <vzykova23@gmail.com>
2024-12-11 03:53:08 +0200

    Add files via upload

---
+++ дз 1.1/1.2/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+for (int i = 0; n >= m; i++)
+{
+    n = n - m;
+    Console.WriteLine(i);
+}
---
+++ дз 1.1/дз 1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿int chislo = Convert.ToInt32(Console.ReadLine());
+for (int count = 0, i = 1; i < chislo; i++)
+{
+    for (int k = 1; k <= i; k++)
+    {
+        if (i % k == 0) count = count + 1;
+    }
+    if (count == 2)
+    {
+        Console.WriteLine(i);
+    }
+    count = 0;
+}
