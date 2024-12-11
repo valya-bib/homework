e2b206719b2b4c026729bbefecd568a501d9c077
valya-bib <vzykova23@gmail.com>
2024-12-11 03:57:35 +0200

    Add files via upload

---
+++ дз 3.1/дз 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        string binary1 = Convert.ToString(num1, 2).PadLeft(32, '0');
+        string binary2 = Convert.ToString(num2, 2).PadLeft(32, '0');
+
+        Console.WriteLine(binary1);
+        Console.WriteLine(binary2);
+
+        int sum = num1 + num2;
+        string binarySum = Convert.ToString(sum, 2).PadLeft(32, '0');
+
+        Console.WriteLine(binarySum);
+
+        Console.WriteLine(sum);
+    }
+}
