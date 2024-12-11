c44475eb79707f88cd902bf23116200a2d074820
valya-bib <vzykova23@gmail.com>
2024-12-11 03:58:54 +0200

    Add files via upload

---
+++ дз 11.01/дз 11.01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "f.txt";
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("x sin(x)");
+            for (double x = 0; x <= 1; x += 0.1)
+            {
+                double sinValue = Math.Sin(x);
+                writer.WriteLine($"{x:F1} {sinValue:F4}");
+            }
+        }
+
+        Console.WriteLine($"Таблица значений sin(x) записана в файл {filePath}.");
+    }
+}
